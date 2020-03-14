c6deefe066b55c1054d9cb1bbd45d7927d22d64e
Fernando Ultremare <feroult@gmail.com>
2020-03-14 18:41:51 -0300

    covid19 study

--- bigquery/create_table.sh
+++ bigquery/create_table.sh
@@ -2,8 +2,6 @@
 
 SPREADSHEET="1avGWWl1J19O_Zm0NGTGy2E-fOG05i4ljRfjl87P7FiA"
 
-#SCHEMA="country_region:STRING,province_state:STRING,date:DATE,case_type:STRING,cases:INTEGER,long:NUMERIC,lat:NUMERIC,difference:INTEGER,last_update_date:DATETIME"
-
 TABLE="covid19.cases"
 
 bq rm -f -t ${TABLE}
