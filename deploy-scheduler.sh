12a335fe1bf24b5603f30a27a7ee33d34840bad0
Fernando Ultremare <feroult@gmail.com>
2020-03-16 12:40:14 -0300

    covid19 study

---
+++ deploy-scheduler.sh
@@ -0,0 +1,26 @@
+#!/bin/sh -xe
+
+PROJECT_ID="feroult-cloud"
+JOB_ID="update-covid19"
+URI="https://covid19outbreak-d3uwkj73ia-uc.a.run.app"
+CLIENT_SERVICE_ACCOUNT_EMAIL="${JOB_ID}@${PROJECT_ID}.iam.gserviceaccount.com "
+
+create_service_account() {
+  gcloud iam service-accounts create ${JOB_ID}
+
+  gcloud projects add-iam-policy-binding ${PROJECT_ID} \
+    --member serviceAccount:${CLIENT_SERVICE_ACCOUNT_EMAIL} \
+    --role roles/run.invoker
+}
+
+create_scheduler() {
+  gcloud scheduler jobs delete ${JOB_ID}
+  gcloud scheduler jobs create http ${JOB_ID} \
+    --http-method=GET \
+    --schedule="every 10 minutes" \
+    --uri=${URI} \
+    --oidc-service-account-email=${CLIENT_SERVICE_ACCOUNT_EMAIL}
+}
+
+# create_service_account
+create_scheduler  
