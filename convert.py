ce4720d20c8b5c756e7019046673263fe373a540
Fernando Ultremare <feroult@gmail.com>
2020-03-16 21:16:55 -0300

    covid19 study

--- convert.py
+++ convert.py
@@ -15,6 +15,9 @@ writer = csv.writer(sys.stdout, delimiter=',', quotechar='"', quoting=csv.QUOTE_
 def format_date(d):
     return datetime.datetime.strptime(d, '%m/%d/%y').strftime('%Y-%m-%d')
 
+def format_float(s):
+    return '{0:.4f}'.format(float(s))
+
 for row in csv.reader(iter(sys.stdin.readline, ''), delimiter=',', quotechar='"'):
     base = row[0:BASE_COLS]
     if isheader:
@@ -22,6 +25,8 @@ for row in csv.reader(iter(sys.stdin.readline, ''), delimiter=',', quotechar='"'
         dates = [format_date(d) for d in row[BASE_COLS:]]
         isheader = False
     else:
+        base[2] = format_float(base[2])
+        base[3] = format_float(base[3])
         for i in range(BASE_COLS,len(row)):
             date = dates[i-BASE_COLS]
             count = row[i]
